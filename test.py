b8996308b66710a66d2fc2b75402171826dd7be1
lpj-10 <lpj20050708@163.com>
2025-06-15 00:59:58 +0800

    Remove ide options and test from git

--- test.py
+++
@@ -1,9 +0,0 @@
-import AI_analyse
-import chess
-
-from AI_analyse import *
-
-deepseek_analyse(chess.Board("r5k1/1p1r1pp1/1Bn1p2p/3n1b2/2RP4/4PN2/P3BPPP/2R3K1 w - - 1 20"))
-#print(board_to_prompt_string(chess.Board("r1b2rk1/4b1pp/p3pB2/1p1p1nN1/7P/2N3P1/PP3PB1/2R1R1K1 b - - 0 20")))
-#print(board_to_prompt_string(chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")))
-
